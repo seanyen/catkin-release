8e8f4d97fd1c2300f0b8f83766bfc48b7236fb12
Tully Foote <tfoote@willowgarage.com>
2012-10-11 23:27:32 -0700

    Rebase from release/catkin

--- cmake/parse_package_xml.py
+++ cmake/parse_package_xml.py
@@ -2,20 +2,34 @@
 
 from __future__ import print_function
 import sys
+import argparse
 
 from catkin_pkg.package import parse_package
 
-package = parse_package(sys.argv[1])
 
-values = {}
-values['VERSION'] = '"%s"' % package.version
+def main():
+    """
+    Reads package.xml and writes extracted variables to stdout.
+    """
+    parser = argparse.ArgumentParser(description="Read package.html and write extracted variables to stdout")
+    parser.add_argument('package_xml')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
+    package = parse_package(args.package_xml)
 
-values['MAINTAINER'] = '"%s"' % (', '.join([str(m) for m in package.maintainers]))
+    values = {}
+    values['VERSION'] = '"%s"' % package.version
 
-values['BUILD_DEPENDS'] = ' '.join(['"%s"' % str(d) for d in package.build_depends])
-values['RUN_DEPENDS'] = ' '.join(['"%s"' % str(d) for d in package.run_depends])
+    values['MAINTAINER'] = '"%s"' % (', '.join([str(m) for m in package.maintainers]))
 
-with open(sys.argv[2], 'w') as ofile:
-    print(r'set(_CATKIN_CURRENT_PACKAGE "%s")' % package.name, file=ofile)
-    for k, v in values.items():
-        print('set(%s_%s %s)' % (package.name, k, v), file=ofile)
+    values['BUILD_DEPENDS'] = ' '.join(['"%s"' % str(d) for d in package.build_depends])
+    values['RUN_DEPENDS'] = ' '.join(['"%s"' % str(d) for d in package.run_depends])
+
+    with open(args.outfile, 'w') as ofile:
+        print(r'set(_CATKIN_CURRENT_PACKAGE "%s")' % package.name, file=ofile)
+        for k, v in values.items():
+            print('set(%s_%s %s)' % (package.name, k, v), file=ofile)
+
+
+if __name__ == '__main__':
+    main()
